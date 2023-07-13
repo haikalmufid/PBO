3853976069d1977a6916173fd53280fcd4949d69
haikalmufid <94338565+haikalmufid@users.noreply.github.com>
2023-07-13 22:02:16 +0700

    Update Pesanan.java

--- UAS/Pesanan.java
+++ UAS/Pesanan.java
@@ -4,6 +4,10 @@ public class Pesanan {
     private double harga;
     private String alamatPengiriman;
 
+    public Pesanan() {
+        // Konstruktor default
+    }
+
     public Pesanan(String namaBarang, int jumlah, double harga, String alamatPengiriman) {
         this.namaBarang = namaBarang;
         this.jumlah = jumlah;
@@ -15,21 +19,38 @@ public class Pesanan {
         return namaBarang;
     }
 
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
     public int getJumlah() {
         return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
     }
 
     public double getHarga() {
         return harga;
     }
 
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
     public String getAlamatPengiriman() {
         return alamatPengiriman;
     }
 
+    public void setAlamatPengiriman(String alamatPengiriman) {
+        this.alamatPengiriman = alamatPengiriman;
+    }
+
     public void printPesanan() {
-        System.out.println("Nama Barang : " + namaBarang);
-        System.out.println("Jumlah : " + jumlah);
-        System.out.println("Harga : " + harga);
+        System.out.println("Nama Barang: " + namaBarang);
+        System.out.println("Jumlah: " + jumlah);
+        System.out.println("Harga: " + harga);
     }
 }
+
